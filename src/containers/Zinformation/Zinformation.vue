8a7e5583b2af8a2ffa2530d8618b2bd16836c517
Baby123xi <mr.zp@foxmail.com>
2017-10-13 15:18:40 +0800

    list

---
+++ src/containers/Zinformation/Zinformation.vue
@@ -0,0 +1,23 @@
+<template>
+   
+</template>
+
+<script>
+import Zlist  from '../../components/Zlist/Zlist'
+export default {
+  name: 'Layout',
+  data () {
+    return {
+      
+    }
+  },
+  comments: {
+    Zlist
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+@import "../../assets/style/base.scss";
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Layout from '@/containers/Layout/Layout'
 import Home from '@/containers/Home/Home'
+import Zinformation from '@containers/Zinformation/Zinformation'
 Vue.use(Router)
 
 export default new Router({
@@ -16,6 +17,11 @@ export default new Router({
           path: '/home',
           name: 'home',
           component: Home
+        },
+        {
+          path: '/zinformation',
+          name: 'zinformation',
+          component: Zinformation
         }
       ]
      }

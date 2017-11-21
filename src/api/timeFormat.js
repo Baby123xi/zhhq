0eaea2872ea607d5eac9ab7293f3bec3691f955f
Baby123xi <mr.zp@foxmail.com>
2017-11-21 15:33:23 +0800

    update

--- src/api/timeFormat.js
+++ src/api/timeFormat.js
@@ -9,4 +9,4 @@ export function getDate(shijianchuo, fgf) {
 //   var s = time.getSeconds();  
 //   return y+'-'+add0(m)+'-'+add0(d)+' '+add0(h)+':'+add0(mm)+':'+add0(s);  
   return y+ fgf +add0(m)+ fgf +add0(d); 
-};  
+}; 
